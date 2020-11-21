875524b8df3d6c70319375a628d3dd5e052b7551
Vitalij Robinovsky <vitalij.robin@gmail.com>
2020-11-21 12:17:06 +0300

    Added account processing without statistics

--- app/src/main/java/robin/vitalij/fortniteassitant/repository/network/GetUserRepository.kt
+++ app/src/main/java/robin/vitalij/fortniteassitant/repository/network/GetUserRepository.kt
@@ -10,13 +10,16 @@ import robin.vitalij.fortniteassitant.model.network.stats.PlayerMatchesResponse
 import robin.vitalij.fortniteassitant.model.network.stats.PlayerStatsResponse
 import javax.inject.Inject
 
+private const val TIME_WINDOW = "lifetime"
+private const val TYPE_IMAGE = "all"
+
 class GetUserRepository @Inject constructor(
     private val fortniteRequestsIOApi: FortniteRequestsIOApi,
     private val fortniteRequestsComApi: FortniteRequestsComApi
 ) {
 
     fun getUser(playerId: String): Single<FortniteProfileResponse> {
-        val playerStats = fortniteRequestsComApi.getStats(playerId, "lifetime", "all")
+        val playerStats = fortniteRequestsComApi.getStats(playerId, TIME_WINDOW, TYPE_IMAGE)
         val matchesPlayerStats = fortniteRequestsIOApi.getMatches(playerId)
 
         return Single.zip(
